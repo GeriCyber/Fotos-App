e602de0c16db018e1f5c3fe3cb587ba6501807d5
Geraldine Herrera Garmendia <gericyber@gmail.com>
2019-03-05 13:03:04 -0400

    production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBfnr4ltGElVQRxJLiV8afgzaNROR2KbCk',
+    authDomain: 'fir-fotos-ea84c.firebaseapp.com',
+    databaseURL: 'https://fir-fotos-ea84c.firebaseio.com',
+    projectId: 'fir-fotos-ea84c',
+    storageBucket: 'fir-fotos-ea84c.appspot.com',
+    messagingSenderId: '599786105140'
+  }
 };
